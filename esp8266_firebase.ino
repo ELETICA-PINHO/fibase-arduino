f812d414a754931fbd341628d6b50aeecbce6118
ELETICA-PINHO <50990384+ELETICA-PINHO@users.noreply.github.com>
2019-07-14 07:41:57 -0300

    Create esp8266_firebase.ino

---
+++ esp8266_firebase.ino
@@ -0,0 +1,128 @@
+#include <ESP8266WiFi.h>
+#include <FirebaseArduino.h>
+
+#define WIFI_SSID "SERVIDOR"
+#define WIFI_PASSWORD "#EletricaPinho#"
+
+#define FIREBASE_HOST  "teste-lampada-3e722.firebaseio.com"
+#define FIREBASE_AUTH  "HUIWqbI8wAIWZQfSyRbGpaZ8fuiydK0p5CJ3k8gr"
+
+//#define FIREBASE_HOST  "lampadaquarto.firebaseio.com"
+//#define FIREBASE_AUTH  "3eqd4TavzXsC9GgxER3BMwipTJBdNrKnKp1KHDL3"
+
+
+#define  PULSADOR1   2
+#define  RELE1       0
+int StatusPulsador1 ;
+int StatusRele = 0;
+int ledStatus ;
+
+//---------------------------prototipoi de função -----------------------
+void FUNCAO_PULSADOR1();
+
+
+//--------------------------- fim prototipoi de função -----------------------
+
+void setup() 
+{
+  Serial.begin(115200);
+
+  // connect to wifi.
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  Serial.print("connecting");
+  while (WiFi.status() != WL_CONNECTED) 
+  {
+    Serial.print(".");
+    delay(500);
+  }
+  Serial.println();
+  Serial.print("connected: ");
+  Serial.println(WiFi.localIP());
+
+
+  Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
+  pinMode(RELE1,OUTPUT);
+  digitalWrite(RELE1, HIGH);
+
+}
+
+
+
+void loop() 
+{
+  ledStatus = Firebase.getInt("placateste");
+  //int pulsador = Firebase.getInt("");
+
+    StatusRele = digitalRead(RELE1);
+    Firebase.setInt("stausplacateste", StatusRele);
+
+    FUNCAO_PULSADOR1();
+
+    delay(200);
+
+
+
+    switch (ledStatus) 
+    {
+        case 1:
+          Serial.println(ledStatus);
+          digitalWrite(RELE1, HIGH);
+          delay(100);
+          break;
+
+        case 0:
+          Serial.println(ledStatus);
+          digitalWrite(RELE1, LOW);
+          delay(100);
+          break;
+
+        default:
+        Serial.println("Diferente de 0 e 1");
+          
+    }
+
+
+}
+
+
+// funções ----------------------------------------------------------------------------------------------
+
+/// ---------------------------- função pulsador --------------------------------------------------------
+
+void FUNCAO_PULSADOR1() {
+
+
+
+  if ( (digitalRead(PULSADOR1) == LOW)) 
+  {
+
+    if (StatusPulsador1 == LOW) 
+    {
+      //digitalWrite(RELE1, !digitalRead(RELE1));
+     // delay(300);
+
+      ledStatus = Firebase.getInt("placateste");
+
+      delay(100);
+
+      switch (ledStatus) 
+    {
+        case 1:
+          Serial.println("Rele pulsador  0");
+          Firebase.setInt("placateste", 0);
+          delay(100);
+          break;
+
+        case 0:
+           Serial.println("Rele pulsador  1");
+          Firebase.setInt("placateste", 1);
+          delay(100);
+          break;
+       
+    
+     }
+    }
+  }
+}
+
+/// ----------------------------  fim função pulsador --------------------------------------------------------
